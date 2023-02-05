d1e2470a0586cf967c4cc9b034066934962470aa
GulYALCIN38 <gulfen3@gmail.com>
2023-02-05 21:22:24 +0100

    son

--- src/test/java/techproed/tests/smoketests/Day22_PositiveLoginTest.java
+++ src/test/java/techproed/tests/smoketests/Day22_PositiveLoginTest.java
@@ -1,5 +1,6 @@
 package techproed.tests.smoketests;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import techproed.pages.BlueRentalHomePage;
 
@@ -22,14 +23,19 @@ public class Day22_PositiveLoginTest {
     BlueRentalLoginPage blueRentalLoginPage;
     @Test
     public void US100201_Admin_Login(){
+        Reporter.log("SAYFAYA GIT ");
         Driver.getDriver().get(ConfigReader.getProperty("app_url"));
         blueRentalHomePage = new BlueRentalHomePage();
         blueRentalLoginPage = new BlueRentalLoginPage();
+        Reporter.log("LOGIN TIKLA");
         blueRentalHomePage.loginLink.click();
+        Reporter.log("GIRIS BILGILERI GIR");
         blueRentalLoginPage.emailBox.sendKeys(ConfigReader.getProperty("admin_email"));
         blueRentalLoginPage.passwordBox.sendKeys(ConfigReader.getProperty("admin_sifre"));
         blueRentalLoginPage.loginButton.click();
         Assert.assertTrue(blueRentalHomePage.userID.isDisplayed());
+        Reporter.log("GIRIS YAPIDIGINI DOGRULA");
         Driver.closeDriver();
+        Reporter.log("DRIVERI KAPAT");
     }
 }
